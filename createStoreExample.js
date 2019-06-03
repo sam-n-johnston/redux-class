910247537e13529dbcb017b8eac15e6e672c0f0f
Sam-Nicolai Johnston <sam-nicolai.johnston@ssense.com>
2019-06-03 09:47:29 -0400

    Basic example

---
+++ createStoreExample.js
@@ -0,0 +1,91 @@
+// state is read-only immutable
+const state = {
+    counter: 10,
+};
+
+// state can only be changed by action
+dispatch({
+    type: 'INCREMENT'
+})
+
+// Pure vs impure function
+const square = (x) => x * x
+
+let timer = 0;
+
+const squareIfUnder10 = (x) => {
+    if (timer < 10) {
+        timer++
+        return x * x
+    }
+    else return x
+}
+
+// Reducer to describe state changes
+const reducer = (state = { counter: 0 }, action) => {
+    if(action.type === 'INCREMENT') return { counter: state.counter + 1 }
+    if(action.type === 'DECREMENT') return { counter: state.counter - 1 }
+
+    return state
+}
+
+// Create basic redux usage example 
+expect(
+    reducer({ counter: 0 }, { type: 'INCREMENT' })
+).toEqual({ counter: 1 })
+expect(
+    reducer({ counter: 1 }, { type: 'INCREMENT' })
+).toEqual({ counter: 2 })
+expect(
+    reducer({ counter: 0 }, { type: 'DECREMENT' })
+).toEqual({ counter: -1 })
+expect(
+    reducer({ counter: 1 }, { type: 'DECREMENT' })
+).toEqual({ counter: 0 })
+expect(
+    reducer(undefined, { type: 'INCREMENT' })
+).toEqual({ counter: 1 })
+
+console.log('TESTS PASSED')
+
+// Create `createStore` function
+const createStore = (reducer) => {
+    let state;
+    const listeners = [];
+
+    const getState = () => state;
+
+    const subscribe = (listener) => {
+        listeners.push(listener);
+    }
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach(listener => listener());
+    }
+
+    return { getState, subscribe, dispatch }
+}
+
+
+// Create async usage
+
+
+
+// Show conditional listener call (only call if change in object)
+
+
+// Show some sweet debug features
+
+
+
+
+// Show undo/redo
+
+// Show state persistence
+
+
+
+// Talk about boiler plate
+
+
