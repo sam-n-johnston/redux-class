5b4fb96962018f245f4b13b82ee71176d58eb051
Sam-Nicolai Johnston <sam-nicolai.johnston@ssense.com>
2019-06-04 09:42:46 -0400

    Add fetch from server as middleware

--- 10 - middleware2.js
+++ 10 - middleware2.js
@@ -1,15 +1,24 @@
 const state = {
     counter: 10,
 };
+// const increment = dispatch => {
+//     dispatch({ type: 'SENDING_REQUEST' })
+//     return new Promise(resolve => setTimeout(resolve, 2000))
+//         .then(() => dispatch({ type: 'INCREMENT' }))
+//         .catch(() => dispatch({ type: 'REQUEST_FAILED' }))
+// }
 const increment = {
-    type: 'INCREMENT'
+    type: 'FETCH_INCREMENT',
+    payload: {
+        endpoint: 'https://httpbin.org/ip'
+    }
 }
 const decrement = {
     type: 'DECREMENT'
 }
 const reducer = (state = { counter: 0 }, action) => {
     if(state.counter > 10) throw new Error('BOOM')
-    if(action.type === 'INCREMENT') return { counter: state.counter + 1 }
+    if(action.type === 'FETCH_INCREMENT_SUCCESS') return { counter: state.counter + 1 }
     if(action.type === 'DECREMENT') return { counter: state.counter - 1 }
 
     return state
@@ -42,7 +51,23 @@ const crashReporter = store => next => action => {
   }
 }
 
-const store = createStore(reducer, applyMiddleware(crashReporter, history, logger))
+const fetchFromServer = store => next => action => {
+    if (action.payload && action.payload.endpoint){
+        store.dispatch({ type: `${action.type}_REQUEST` })
+        fetch(action.payload.endpoint)
+            .then((res) => store.dispatch({ type: `${action.type}_SUCCESS`, payload: res }))
+            .catch((err) => store.dispatch({ type: `${action.type}_FAILURE`, payload: err }))
+    }
+    return next(action)
+}
+
+
+const thunk = store => next => action => {
+    if (typeof action === 'function') return action(store.dispatch, store.getState)
+    return next(action)
+}
+
+const store = createStore(reducer, applyMiddleware(crashReporter, history, logger, thunk, fetchFromServer))
 
 const render = () => {
     document.getElementById("renderDiv").innerHTML = `
